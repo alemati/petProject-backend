d03d68202cc4ed69c3b628f662a87c3a04fd88be
Aleksander <matikainen.aleksander@gmail.com>
2021-02-01 21:16:20 +0200

    heroku fix

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ const skillsRouter = require('./controllers/skills')
 app.use('/api/skills', skillsRouter)
 
 // const url = config.MONGODB_URI
-// let url = process.env.MONGODB_URI
-const url = "mongodb+srv://AM:fsdb@cluster0.sbnjq.mongodb.net/petProject?retryWrites=true&w=majority"
+let url = process.env.MONGODB_URI
+// const url = "mongodb+srv://AM:fsdb@cluster0.sbnjq.mongodb.net/petProject?retryWrites=true&w=majority"
 console.log('connecting to', url)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(result => {
